b91ddfb325a580ecb7b85883a8fc5bc6a875c287
Keith <Keith@Keith-PC>
2012-11-04 13:54:56 -0800

    Added Player.java class
    Signed-off-by: Keith <Keith@Keith-PC>

---
+++ BackToSchool/src/Player.java
@@ -0,0 +1,45 @@
+
+public class Player {
+	double sciRigor;
+	double creativity;
+	double quantReasoning;
+	
+	// player starts with 1.0 GPA statistics (debatable)
+	public Player(){
+		sciRigor=1.0;
+		creativity=1.0;
+		quantReasoning=1.0;
+	}
+	
+	// increase the statistic
+	public void increaseSciRigor(double inc){
+		sciRigor+=inc;
+	}
+	
+	// increase the statistic
+	public void increaseCreativit(double inc){
+		creativity+=inc;
+	}
+	
+	// increase the statistic
+	public void increaseQuantReasoning(double inc){
+		quantReasoning+=inc;
+	}
+	
+	//return the statistic
+	public double getSciRigor(){
+		return sciRigor;
+	}
+	
+	//return the statistic
+	public double getCreativity(){
+		return creativity;
+	}
+	
+	//return the statistic
+	public double getQuantReasoning(){
+		return quantReasoning;
+	}
+	
+
+}
