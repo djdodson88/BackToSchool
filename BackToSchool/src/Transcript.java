042ae06c7b56681c72794add149723f8fb0c329c
Keith <Keith@Keith-PC>
2012-11-25 21:08:31 -0800

    Finished transcript with background provide by Jackie
    Signed-off-by: Keith <Keith@Keith-PC>

--- BackToSchool/src/Transcript.java
+++ BackToSchool/src/Transcript.java
@@ -1,7 +1,12 @@
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -14,33 +19,50 @@ public class Transcript extends JPanel{
 	JLabel creativityLabel;
 	JLabel sciRigorLabel;
 	JLabel quantReasLabel;
+	JLabel averageGPA;
+	ImageIcon background;
 	
 	public Transcript(Player player, Day day){
 		this.setPreferredSize(new Dimension(800, 600));// setting the size
 		this.setBackground(Color.white);// color of background
 		
-		dayLabel = new JLabel("Day "+day.getDay());
-		dayLabel.setBounds(0,0,50,50);
+		dayLabel = new JLabel("Day "+day.getDay()+" Transcript");
+		dayLabel.setFont(new Font("Serif", Font.PLAIN, 30));
+		dayLabel.setBounds(300,0,300,100);
 		
-		creativityLabel = new JLabel(Double.toString(player.getCreativity()));
-		creativityLabel.setBounds(100,100,50,50);
+		creativityLabel = new JLabel("Creativity: "+Double.toString(player.getCreativity()));
+		creativityLabel.setBounds(300,100,300,100);
+		creativityLabel.setFont(new Font("Serif", Font.PLAIN, 20));
 		
-		sciRigorLabel = new JLabel(Double.toString(player.getSciRigor()));
-		sciRigorLabel.setBounds(100,200,50,50);
+		sciRigorLabel = new JLabel("Scientific Rigor: "+Double.toString(player.getSciRigor()));
+		sciRigorLabel.setBounds(300,180,300,100);
+		sciRigorLabel.setFont(new Font("Serif", Font.PLAIN, 20));
 		
-		quantReasLabel = new JLabel(Double.toString(player.getQuantReasoning()));
-		quantReasLabel.setBounds(100,300,50,50);
+		quantReasLabel = new JLabel("Quantitative Reasoning: "+Double.toString(player.getQuantReasoning()));
+		quantReasLabel.setBounds(300,260,300,100);
+		quantReasLabel.setFont(new Font("Serif", Font.PLAIN, 20));
 		
-		
+		double aveGPA = (player.getCreativity()+player.getSciRigor()+player.getQuantReasoning())/3;
+		averageGPA = new JLabel("Average GPA: "+ aveGPA);
+		averageGPA.setBounds(300,350,300,100);
+		averageGPA.setFont(new Font("Serif", Font.PLAIN, 25));
+		background = new ImageIcon("art/transcript/transcript_bg.jpg");
 		
 		
 		this.add(dayLabel);
+		this.add(averageGPA);
 		this.add(creativityLabel);
 		this.add(sciRigorLabel);
 		this.add(quantReasLabel);
 		
 		setLayout(null);
 		setVisible(true);
+		
+	}
+	
+	public void paintComponent(Graphics g)
+	{
+		background.paintIcon(this,g,0,0);
 	}
 	
 	public static void main(String[] args) {
@@ -53,7 +75,11 @@ public class Transcript extends JPanel{
 		frame.pack();
 		
 		JButton exitButton = new JButton("Exit");
-		exitButton.setBounds(100,500,100,50);
+		exitButton.setBounds(350,500,100,50);
+		exitButton.setBackground(null);
+		exitButton.setOpaque(false);
+	//	exitButton.setBorder(null);
+		
 		exitButton.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e)
 			{
